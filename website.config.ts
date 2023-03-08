3dc2f6c023f37954ffb0e2a5d8c48b9dd8323802
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-03-08 11:46:48 +0700

    fixed themes

--- website.config.ts
+++ website.config.ts
@@ -5,6 +5,7 @@ import { iconConfig } from '@reusable-ui/components'
 
 defineTheme('primary', 'hsl(28, 60%, 10%)');
 defineTheme('secondary', 'hsl(28, 30%, 80%)');
+console.log('color themes registered!');
 
 iconConfig.image.files.push(
     { name: 'artswimwear.svg', ratio: '48/40' },
