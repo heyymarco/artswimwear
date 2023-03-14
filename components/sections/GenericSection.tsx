689a577d3ff999e3897a98652cc3bbb8c658959b
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-03-14 16:34:47 +0700

    updated <GenericSection>

--- components/sections/GenericSection.tsx
+++ components/sections/GenericSection.tsx
@@ -1,20 +1,55 @@
 import { default as React, createContext } from 'react'
-import { Container, ContainerProps } from '@reusable-ui/components'
+import { Container, ContainerProps, ControlProps } from '@reusable-ui/components'
 
 
 
-export interface GenericSectionProps extends ContainerProps {
+export interface GenericSectionProps
+    extends
+        ContainerProps,
+        ControlProps // fake support for <AccordionItem>'s contentComponent
+{
 }
 /**
  * A generic `<section>` without any `<h1-h6>` or `<article>`.
  * You should manually including at least one `<article>` with appropriate `<h1-h6>`.
  */
 export const GenericSection = (props: GenericSectionProps) => {
+    // rest props:
+    const {
+        //#region <Control> props
+        // accessibilities:
+        tabIndex           : _tabIndex,           // remove
+        
+        
+        
+        // states:
+        focused            : _focused,            // remove
+        assertiveFocusable : _assertiveFocusable, // remove
+        arrived            : _arrived,            // remove
+        //#endregion <Control> props
+        
+        
+        
+        //#region <Indicator> props
+        // states:
+        enabled         : _enabled,         // remove
+        inheritEnabled  : _inheritEnabled,  // remove
+        
+        active          : _active,          // remove
+        inheritActive   : _inheritActive,   // remove
+        
+        readOnly        : _readOnly,        // remove
+        inheritReadOnly : _inheritReadOnly, // remove
+        //#endregion <Indicator> props
+    ...restContainerProps} = props; // fake support for <AccordionItem>'s contentComponent
+    
+    
+    
     // jsx:
     return (
         <Container
             // rest props:
-            {...props}
+            {...restContainerProps}
             
             
             
