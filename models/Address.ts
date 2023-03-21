81897923dca123fd45b6dcde591beb61f283caf3
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-03-21 22:06:22 +0700

    updated schema

--- models/Address.ts
+++ models/Address.ts
@@ -3,15 +3,15 @@ import { Schema } from 'mongoose'
 
 
 export const addressSchema = new Schema({
-    firstName : { type: String , required: true  },
-    lastName  : { type: String , required: true  },
+    firstName : { type: String , required: true  , minLength: 2, maxLength: 30 },
+    lastName  : { type: String , required: true  , minLength: 1, maxLength: 30 },
     
-    phone     : { type: String , required: true  },
-    email     : { type: String , required: true  },
+    phone     : { type: String , required: true  , minLength: 5, maxLength: 15 },
+    email     : { type: String , required: true  , minLength: 5, maxLength: 50 },
     
-    address   : { type: String , required: true  },
-    city      : { type: String , required: true  },
-    zone      : { type: String , required: true  },
-    zip       : { type: String , required: false },
-    country   : { type: String , required: true  },
+    address   : { type: String , required: true  , minLength: 5, maxLength: 90 },
+    city      : { type: String , required: true  , minLength: 5, maxLength: 50 },
+    zone      : { type: String , required: true  , minLength: 5, maxLength: 50 },
+    zip       : { type: String , required: false , minLength: 2, maxLength: 11 },
+    country   : { type: String , required: true  , minLength: 2, maxLength:  3 },
 });
