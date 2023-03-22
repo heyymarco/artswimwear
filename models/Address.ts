fed7dbbd31bfe417ef4517ad917db59c095bdd34
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-03-22 10:43:40 +0700

    updated model

--- models/Address.ts
+++ models/Address.ts
@@ -7,11 +7,10 @@ export const addressSchema = new Schema({
     lastName  : { type: String , required: true  , minLength: 1 , maxLength: 30 },
     
     phone     : { type: String , required: true  , minLength: 5 , maxLength: 15 },
-    email     : { type: String , required: true  , minLength: 5 , maxLength: 50 },
     
     address   : { type: String , required: true  , minLength: 5 , maxLength: 90 },
-    city      : { type: String , required: true  , minLength: 5 , maxLength: 50 },
-    zone      : { type: String , required: true  , minLength: 5 , maxLength: 50 },
+    city      : { type: String , required: true  , minLength: 3 , maxLength: 50 },
+    zone      : { type: String , required: true  , minLength: 3 , maxLength: 50 },
     zip       : { type: String , required: false , minLength: 2 , maxLength: 11 },
     country   : { type: String , required: true  , minLength: 2 , maxLength:  3 },
 });
