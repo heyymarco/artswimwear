31baed7cafd817fa5e08a8c9caabf0925e7ea0e4
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-03-22 10:50:38 +0700

    updated models

--- models/Product.ts
+++ models/Product.ts
@@ -6,6 +6,7 @@ const productSchema = new Schema({
     name           : { type: String, required: true  },
     price          : { type: Number, required: true  },
     shippingWeight : { type: Number, required: false },
+    stock          : { type: Number, required: false },
     description    : String,
     images         : [String],
     path           : String,
