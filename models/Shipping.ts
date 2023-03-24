747268c138ba22b723b504752346c70300c80e57
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-03-24 14:27:06 +0700

    tidy up code

--- models/Shipping.ts
+++ models/Shipping.ts
@@ -11,35 +11,35 @@ const coverageCitySchema = new Schema({
     city           : { type: String                  , required: true  , minLength: 3 , maxLength: 50 },
     
     estimate       : { type: String                  , required: false , minLength: 2 , maxLength: 50 },
-    shippingRates  : { type: [shippingRateSchema]    , required: true  },
+    shippingRates  : { type: [shippingRateSchema]    , required: true                                 },
 });
 const coverageZoneSchema = new Schema({
     zone           : { type: String                  , required: true  , minLength: 3 , maxLength: 50 },
     
     estimate       : { type: String                  , required: false , minLength: 2 , maxLength: 50 },
-    shippingRates  : { type: [shippingRateSchema]    , required: true  },
+    shippingRates  : { type: [shippingRateSchema]    , required: true                                 },
     
-    cities         : { type: [coverageCitySchema]    , required: false },
+    cities         : { type: [coverageCitySchema]    , required: false                                },
 });
 const coverageCountrySchema = new Schema({
     country        : { type: String                  , required: true  , minLength: 2 , maxLength:  3 },
     
     estimate       : { type: String                  , required: false , minLength: 2 , maxLength: 50 },
-    shippingRates  : { type: [shippingRateSchema]    , required: true  },
+    shippingRates  : { type: [shippingRateSchema]    , required: true                                 },
     
-    zones          : { type: [coverageZoneSchema]    , required: false },
+    zones          : { type: [coverageZoneSchema]    , required: false                                },
 });
 
 const shippingSchema = new Schema({
-    enabled        : { type: Boolean                 , required: true  },
+    enabled        : { type: Boolean                 , required: true                                 },
     name           : { type: String                  , required: true  , minLength: 2 , maxLength: 50 },
     
     weightStep     : { type: Number                  , required: true  , min: 0       , max: 100      },
     
     estimate       : { type: String                  , required: false , minLength: 2 , maxLength: 50 },
-    shippingRates  : { type: [shippingRateSchema]    , required: true  },
+    shippingRates  : { type: [shippingRateSchema]    , required: true                                 },
     
-    countries      : { type: [coverageCountrySchema] , required: false },
+    countries      : { type: [coverageCountrySchema] , required: false                                },
 });
 
 export default models.Shipping ?? model('Shipping', shippingSchema);
