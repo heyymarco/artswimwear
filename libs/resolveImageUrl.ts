07aa73a212a9bb586a38fb2e7995a9e005c1383f
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-06-26 23:26:07 +0700

    updated resolveImageUrl

--- libs/resolveImageUrl.ts
+++ libs/resolveImageUrl.ts
@@ -1,7 +1,7 @@
-const resolveImageUrl = (imageId: string|undefined): string|undefined => {
-    if (!imageId) return undefined;
+const resolveImageUrl = <TNull extends undefined|never = never>(imageId: string|TNull): string|TNull => {
+    if (!imageId) return imageId;
     if (imageId.includes('/')) return imageId;
     if (!imageId.includes('.')) return `https://drive.google.com/uc?id=${imageId}`;
-    return `/products/${name}/${imageId}`
+    return `/${imageId}`;
 };
 export default resolveImageUrl;
