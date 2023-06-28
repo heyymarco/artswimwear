8d31b4b79562505af8bc0d00a68678320a967bf9
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-06-28 19:54:36 +0700

    updated next images config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'drive.google.com',
+    remotePatterns: [
+      {
+        protocol : 'https',
+        hostname : 'drive.google.com',
+        port     : '',
+        pathname : '**',
+      },
+      {
+        protocol : 'https',
+        hostname : '**.googleusercontent.com',
+        port     : '',
+        pathname : '**',
+      },
     ],
   },
   
