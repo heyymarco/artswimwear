09d0557331e00efce38fcec68d87b9cffe3953a2
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-07-13 15:23:14 +0700

    update Product model

--- models/Product.ts
+++ models/Product.ts
@@ -3,23 +3,23 @@ import { Schema, Types, models, model, InferSchemaType } from 'mongoose'
 
 
 const productSchema = new Schema({
-    _id            : { type: Types.ObjectId , required: false          },
+    _id            : { type: Types.ObjectId , required: false                },
     
     visibility     : { type: String         , required: true  , enum: ['published', 'hidden', 'draft'] },
     
-    name           : { type: String         , required: true           },
+    name           : { type: String         , required: true                 },
     
-    price          : { type: Number         , required: true  , min: 0 },
-    shippingWeight : { type: Number         , required: false , min: 0 },
+    price          : { type: Number         , required: true  , min: 0       },
+    shippingWeight : { type: Number         , required: false , min: 0       },
     
-    stock          : { type: Number         , required: false , min: 0 },
+    stock          : { type: Number         , required: false , min: 0       },
     
-    path           : { type: String         , required: true           },
+    path           : { type: String         , required: true  , unique: true },
     
-    excerpt        : { type: String         , required: false          },
-    description    : { type: Object         , required: false          },
+    excerpt        : { type: String         , required: false                },
+    description    : { type: Object         , required: false                },
     
-    images         : { type: [String]       , required: false          },
+    images         : { type: [String]       , required: false                },
 });
 export type ProductSchema = InferSchemaType<typeof productSchema>;
 
