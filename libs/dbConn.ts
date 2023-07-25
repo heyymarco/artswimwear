74c385b060edcff6351fd0e32f03376ee0873cc3
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-07-26 00:49:29 +0700

    added prisma

--- libs/dbConn.ts
+++ libs/dbConn.ts
@@ -4,7 +4,7 @@ import mongoose from 'mongoose'
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI!);
+        await mongoose.connect(process.env.DATABASE_URL!);
     }
     catch (error) {
         console.log(error)
