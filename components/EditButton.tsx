866508a47264e97579b5c11c1e5f0ae7559a5fa1
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-10-22 20:52:35 +0700

    added EditButton

---
+++ components/EditButton.tsx
@@ -0,0 +1,54 @@
+// react:
+import {
+    // react:
+    default as React,
+}                           from 'react'
+
+// reusable-ui components:
+import {
+    ButtonIconProps,
+    ButtonIcon,
+}                           from '@reusable-ui/components'      // a set of official Reusable-UI components
+
+
+
+export interface EditButtonProps extends ButtonIconProps
+{
+}
+const EditButton = (props: EditButtonProps) => {
+    // jsx:
+    return (
+        <ButtonIcon
+            // other props:
+            {...props}
+            
+            
+            
+            // appearances:
+            icon={props.icon ?? 'edit'}
+            
+            
+            
+            // variants:
+            size={props.size ?? 'xs'}
+            theme={props.theme ?? 'primary'}
+            buttonStyle={props.buttonStyle ?? 'link'}
+            
+            
+            
+            // classes:
+            className={props.className ?? 'edit'}
+            
+            
+            
+            // accessibilities:
+            title={props.title ?? 'edit'}
+        >
+            {props.children ?? 'Edit'}
+        </ButtonIcon>
+    );
+};
+export {
+    EditButton,
+    EditButton as default,
+}
