924952dee8f38981470da184e21188aa591eb5ee
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-10-23 20:57:09 +0700

    decoupled NavCheckout

--- components/Checkout/Checkout.tsx
+++ components/Checkout/Checkout.tsx
@@ -17,11 +17,6 @@ import {
     default as ReactDOM,
 }                           from 'react-dom'
 
-// redux:
-import {
-    useDispatch,
-}                           from 'react-redux'
-
 // reusable-ui core:
 import {
     // a color management system:
@@ -151,11 +146,11 @@ import {
 import {
     ProgressCheckout,
 }                           from './components/navigations/ProgressCheckout'
+import {
+    NavCheckout,
+}                           from './components/navigations/NavCheckout'
 
 // stores:
-import {
-    showCart,
-}                           from '@/store/features/cart/cartSlice'
 import type {
     PaymentMethod as PaymentMethodType,
 }                           from '@/store/features/checkout/checkoutSlice'
@@ -543,189 +538,6 @@ const CheckoutInternal = (): JSX.Element|null => {
 export {
     Checkout,
     Checkout as default,
-};
-
-
-
-// navigations:
-const NavCheckout = (): JSX.Element|null => {
-    // states:
-    const {
-        // states:
-        checkoutStep,
-        checkoutProgress,
-        
-        isBusy,
-        
-        
-        
-        // actions:
-        gotoStepInformation,
-        gotoStepShipping,
-        gotoPayment,
-    } = useCheckoutState();
-    const isCheckoutFinished = (checkoutStep === 'pending') || (checkoutStep === 'paid');
-    
-    
-    
-    // stores:
-    const dispatch = useDispatch();
-    
-    
-    
-    // utilities:
-    const [prevAction, nextAction] = useMemo(() => {
-        const prevAction = [
-            { text: 'Return to cart'       , action: () => dispatch(showCart(true)) },
-            { text: 'Return to information', action: () => gotoStepInformation()    },
-            { text: 'Return to shipping'   , action: gotoStepShipping               },
-        ][checkoutProgress];
-        
-        const nextAction = [
-            { text: 'Continue to shipping' , action: gotoStepShipping               },
-            { text: 'Continue to payment'  , action: gotoPayment                    },
-        ][checkoutProgress];
-        
-        return [prevAction, nextAction] as const;
-    }, [checkoutProgress]);
-    
-    
-    
-    // jsx:
-    return (
-        <>
-            {!isCheckoutFinished && <>
-                {!!prevAction && <ButtonIcon
-                    // appearances:
-                    icon='arrow_back'
-                    iconPosition='start'
-                    
-                    
-                    
-                    // variants:
-                    size='md'
-                    theme='primary'
-                    buttonStyle='link'
-                    
-                    
-                    
-                    // classes:
-                    className='back'
-                    
-                    
-                    
-                    // accessibilities:
-                    enabled={!isBusy}
-                    
-                    
-                    
-                    // handlers:
-                    onClick={prevAction.action}
-                >
-                    {prevAction.text}
-                </ButtonIcon>}
-                
-                {!!nextAction && <ButtonIcon
-                    // appearances:
-                    icon={!isBusy ? 'arrow_forward' : 'busy'}
-                    iconPosition='end'
-                    
-                    
-                    
-                    // variants:
-                    size='lg'
-                    theme='primary'
-                    gradient={true}
-                    
-                    
-                    
-                    // classes:
-                    className='next'
-                    
-                    
-                    
-                    // accessibilities:
-                    enabled={!isBusy}
-                    
-                    
-                    
-                    // handlers:
-                    onClick={nextAction.action}
-                >
-                    {nextAction.text}
-                </ButtonIcon>}
-            </>}
-            
-            {isCheckoutFinished && <>
-                {/* TODO: remove when the finish order completed */}
-                <ButtonIcon
-                    icon='arrow_back'
-                    iconPosition='start'
-                    
-                    size='md'
-                    theme='primary'
-                    buttonStyle='link'
-                    className='back'
-                    
-                    enabled={!isBusy}
-                    
-                    onClick={gotoPayment}
-                >
-                    BACK
-                </ButtonIcon>
-                {/* TODO: re-activate when the finish order completed */}
-                {/* <p>
-                    <Icon
-                        // appearances:
-                        icon='help'
-                        
-                        
-                        
-                        // variants:
-                        size='md'
-                        theme='primary'
-                    />
-                    Need help?
-                    <Button
-                        // variants:
-                        theme='primary'
-                        buttonStyle='link'
-                    >
-                        <Link href='/contact'>
-                            Contact Us
-                        </Link>
-                    </Button>
-                </p> */}
-                
-                <ButtonIcon
-                    // appearances:
-                    icon='shopping_bag'
-                    iconPosition='end'
-                    
-                    
-                    
-                    // variants:
-                    size='lg'
-                    theme='primary'
-                    gradient={true}
-                    
-                    
-                    
-                    // classes:
-                    className='next'
-                    
-                    
-                    
-                    // accessibilities:
-                    enabled={!isBusy}
-                >
-                    <Link href='/products'>
-                        Continue Shopping
-                    </Link>
-                </ButtonIcon>
-            </>}
-        </>
-    );
 };
 
 
---
+++ components/Checkout/components/navigations/NavCheckout.tsx
@@ -0,0 +1,223 @@
+'use client'
+
+// react:
+import {
+    // react:
+    default as React,
+    
+    
+    
+    // hooks:
+    useMemo,
+}                           from 'react'
+
+// redux:
+import {
+    useDispatch,
+}                           from 'react-redux'
+
+// reusable-ui components:
+import {
+    // simple-components:
+    ButtonIcon,
+}                           from '@reusable-ui/components'      // a set of official Reusable-UI components
+import {
+    Link,
+}                           from '@reusable-ui/next-compat-link'
+
+// stores:
+import {
+    showCart,
+}                           from '@/store/features/cart/cartSlice'
+
+// internals:
+import {
+    useCheckoutState,
+}                           from '../../states/checkoutState'
+
+
+
+// react components:
+const NavCheckout = (): JSX.Element|null => {
+    // states:
+    const {
+        // states:
+        checkoutStep,
+        checkoutProgress,
+        
+        isBusy,
+        
+        
+        
+        // actions:
+        gotoStepInformation,
+        gotoStepShipping,
+        gotoPayment,
+    } = useCheckoutState();
+    const isCheckoutFinished = (checkoutStep === 'pending') || (checkoutStep === 'paid');
+    
+    
+    
+    // stores:
+    const dispatch = useDispatch();
+    
+    
+    
+    // utilities:
+    const [prevAction, nextAction] = useMemo(() => {
+        const prevAction = [
+            { text: 'Return to cart'       , action: () => dispatch(showCart(true)) },
+            { text: 'Return to information', action: () => gotoStepInformation()    },
+            { text: 'Return to shipping'   , action: gotoStepShipping               },
+        ][checkoutProgress];
+        
+        const nextAction = [
+            { text: 'Continue to shipping' , action: gotoStepShipping               },
+            { text: 'Continue to payment'  , action: gotoPayment                    },
+        ][checkoutProgress];
+        
+        return [prevAction, nextAction] as const;
+    }, [checkoutProgress]);
+    
+    
+    
+    // jsx:
+    return (
+        <>
+            {!isCheckoutFinished && <>
+                {!!prevAction && <ButtonIcon
+                    // appearances:
+                    icon='arrow_back'
+                    iconPosition='start'
+                    
+                    
+                    
+                    // variants:
+                    size='md'
+                    theme='primary'
+                    buttonStyle='link'
+                    
+                    
+                    
+                    // classes:
+                    className='back'
+                    
+                    
+                    
+                    // accessibilities:
+                    enabled={!isBusy}
+                    
+                    
+                    
+                    // handlers:
+                    onClick={prevAction.action}
+                >
+                    {prevAction.text}
+                </ButtonIcon>}
+                
+                {!!nextAction && <ButtonIcon
+                    // appearances:
+                    icon={!isBusy ? 'arrow_forward' : 'busy'}
+                    iconPosition='end'
+                    
+                    
+                    
+                    // variants:
+                    size='lg'
+                    theme='primary'
+                    gradient={true}
+                    
+                    
+                    
+                    // classes:
+                    className='next'
+                    
+                    
+                    
+                    // accessibilities:
+                    enabled={!isBusy}
+                    
+                    
+                    
+                    // handlers:
+                    onClick={nextAction.action}
+                >
+                    {nextAction.text}
+                </ButtonIcon>}
+            </>}
+            
+            {isCheckoutFinished && <>
+                {/* TODO: remove when the finish order completed */}
+                <ButtonIcon
+                    icon='arrow_back'
+                    iconPosition='start'
+                    
+                    size='md'
+                    theme='primary'
+                    buttonStyle='link'
+                    className='back'
+                    
+                    enabled={!isBusy}
+                    
+                    onClick={gotoPayment}
+                >
+                    BACK
+                </ButtonIcon>
+                {/* TODO: re-activate when the finish order completed */}
+                {/* <p>
+                    <Icon
+                        // appearances:
+                        icon='help'
+                        
+                        
+                        
+                        // variants:
+                        size='md'
+                        theme='primary'
+                    />
+                    Need help?
+                    <Button
+                        // variants:
+                        theme='primary'
+                        buttonStyle='link'
+                    >
+                        <Link href='/contact'>
+                            Contact Us
+                        </Link>
+                    </Button>
+                </p> */}
+                
+                <ButtonIcon
+                    // appearances:
+                    icon='shopping_bag'
+                    iconPosition='end'
+                    
+                    
+                    
+                    // variants:
+                    size='lg'
+                    theme='primary'
+                    gradient={true}
+                    
+                    
+                    
+                    // classes:
+                    className='next'
+                    
+                    
+                    
+                    // accessibilities:
+                    enabled={!isBusy}
+                >
+                    <Link href='/products'>
+                        Continue Shopping
+                    </Link>
+                </ButtonIcon>
+            </>}
+        </>
+    );
+};
+export {
+    NavCheckout,
+    NavCheckout as default,
+}
