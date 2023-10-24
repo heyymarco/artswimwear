704c9491a5812067912a6c3964fadc619ccb3748
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-10-24 19:16:35 +0700

    updated ProgressCheckout

--- components/Checkout/components/navigations/ProgressCheckout.tsx
+++ components/Checkout/components/navigations/ProgressCheckout.tsx
@@ -6,6 +6,12 @@ import {
     default as React,
 }                           from 'react'
 
+// reusable-ui core:
+import {
+    // an accessibility management system:
+    AccessibilityProvider,
+}                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
+
 // reusable-ui components:
 import {
     // layout-components:
@@ -34,18 +40,24 @@ const ProgressCheckout = (): JSX.Element|null => {
     
     // jsx:
     return (
-        <List
-            // variants:
-            size='sm'
-            theme={!isDesktop ? 'secondary' : 'primary'}
-            outlined={!isDesktop}
-            listStyle='breadcrumb'
-            orientation='inline'
+        <AccessibilityProvider
+            // accessibilities:
+            enabled={true}         // always enabled
+            inheritEnabled={false} // always enabled
         >
-            <ListItem active={checkoutProgress >= 0}>Information</ListItem>
-            <ListItem active={checkoutProgress >= 1}>Shipping</ListItem>
-            <ListItem active={checkoutProgress >= 2}>Payment</ListItem>
-        </List>
+            <List
+                // variants:
+                size='sm'
+                theme={!isDesktop ? 'secondary' : 'primary'}
+                outlined={!isDesktop}
+                listStyle='breadcrumb'
+                orientation='inline'
+            >
+                <ListItem active={checkoutProgress >= 0}>Information</ListItem>
+                <ListItem active={checkoutProgress >= 1}>Shipping</ListItem>
+                <ListItem active={checkoutProgress >= 2}>Payment</ListItem>
+            </List>
+        </AccessibilityProvider>
     );
 };
 export {
