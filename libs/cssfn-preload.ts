739b9a3d1cf5475dc86960855b6582e174a34239
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-10-30 13:27:28 +0700

    updated cssfn-preload

--- libs/cssfn-preload.ts
+++ libs/cssfn-preload.ts
@@ -3,4 +3,5 @@ import { ensureRendererWorkersReady } from '@cssfn/cssfn'
 
 
 // an optional performance tweak: make sure the WebWorker(s) are running before we're busy rendering the DOM:
+// @ts-ignore
 const _loaded = await ensureRendererWorkersReady();
