a5eb42ab392fbd0ba98244950224a1a2fcafb2e9
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-11-05 12:31:09 +0700

    updated images utility

--- libs/images.ts
+++ libs/images.ts
@@ -3,5 +3,6 @@ export const downloadImageAsBase64 = async (url: string, responsiveSize: number,
     if (response.status !== 200) throw Error('Unable to download the image.');
     const blob     = await response.blob();
     const buffer   = Buffer.from(await blob.arrayBuffer());
-    return 'data:image/jpeg;base64, ' + buffer.toString('base64');
+    const contentType = response.headers.get('Content-Type') ?? 'image';
+    return `data:${contentType};base64, ${buffer.toString('base64')}`;
 };
