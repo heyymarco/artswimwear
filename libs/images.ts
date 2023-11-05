4852f126a57974d79a6b05b1bd117adf65fc2311
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-11-05 12:22:22 +0700

    added images utility

---
+++ libs/images.ts
@@ -0,0 +1,7 @@
+export const downloadImageAsBase64 = async (url: string, responsiveSize: number, quality: number = 75): Promise<string> => {
+    const response = await fetch(`${process.env.WEBSITE_URL}/_next/image?url=${encodeURIComponent(url)}&w=${responsiveSize}&q=${quality}`);
+    if (response.status !== 200) throw Error('Unable to download the image.');
+    const blob     = await response.blob();
+    const buffer   = Buffer.from(await blob.arrayBuffer());
+    return 'data:image/jpeg;base64, ' + buffer.toString('base64');
+};
