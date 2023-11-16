4f9686b1c4a949429e83f75aaa477b89944288e2
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-11-16 12:48:04 +0700

    decoupled theme basic

--- theme.config.ts
+++ theme.config.ts
@@ -1,45 +1,17 @@
 import '@reusable-ui/typos/effects'
-import { defineTheme, colorValues } from '@reusable-ui/core'
-import { buttonValues, iconConfig } from '@reusable-ui/components'
+import { iconConfig } from '@reusable-ui/components'
 import { imageValues } from '@heymarco/image'
-import { basicValues } from '@reusable-ui/components'
-
-// other libs:
-import Color                from 'color'                // color utilities
+import './theme.basic.config'
 
 
 
-// light theme
-colorValues.backg = colorValues.light;
-colorValues.foreg = colorValues.dark;
-
-const primaryCol = Color('hsl(185, 35%, 60%)').darken(0.2).saturate(0.2);
-defineTheme('primary',    primaryCol);
-colorValues.primaryText = primaryCol.lighten(1);
-colorValues.primaryMild = primaryCol.lighten(1);
-// colorValues.primaryMild = primaryCol.darken(0.5);
-
-const secondaryCol = primaryCol.rotate(5).desaturate(0.5).lighten(0.65);
-defineTheme('secondary', secondaryCol);
-colorValues.secondaryText = secondaryCol.darken(0.6);
-colorValues.secondaryMild = secondaryCol.lighten(1);
-
-// colorValues.dark = primaryCol.darken(0.5);
-// colorValues.light = primaryCol.lighten(0.5);
-// colorValues.dark = primaryCol.lighten(0.5);
-// colorValues.light = primaryCol.darken(0.5);
-
-
-basicValues.backgGrad = [
-    ['linear-gradient(180deg, transparent, rgba(220, 220, 220, 1)) border-box'],
-];
-basicValues.backgroundBlendMode = 'saturation';
-
+// <Icon>:
 iconConfig.image.files.push(
     { name: 'artswimwear.svg', ratio: '48/40' },
     { name: 'scrolldown.svg', ratio: '20/40' },
 );
 
-buttonValues.backgroundBlendMode = 'saturation';
 
+
+// <Image>:
 imageValues.objectFit = 'cover';
