c14212b8fa181baf9883ea386ea60848249aa46f
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-12-01 09:38:17 +0700

    updated TimezoneEditor

--- components/editors/TimezoneEditor/TimezoneEditor.tsx
+++ components/editors/TimezoneEditor/TimezoneEditor.tsx
@@ -32,59 +32,12 @@ import {
     DropdownListButtonProps,
     DropdownListButton,
 }                           from '@reusable-ui/components'
-
-
-
-// types:
-const possibleValues = [
-    -1      * 60,
-    -10     * 60,
-    -11     * 60,
-    -2      * 60,
-    -3      * 60,
-    -3.5    * 60,
-    -4      * 60,
-    -5      * 60,
-    -6      * 60,
-    -7      * 60,
-    -8      * 60,
-    -9      * 60,
-    -9.5    * 60,
-    0       * 60,
-    1       * 60,
-    10      * 60,
-    10.5    * 60,
-    11      * 60,
-    12      * 60,
-    13      * 60,
-    13.75   * 60,
-    14      * 60,
-    2       * 60,
-    3       * 60,
-    3.5     * 60,
-    4       * 60,
-    4.5     * 60,
-    5       * 60,
-    5.5     * 60,
-    5.75    * 60,
-    6       * 60,
-    6.5     * 60,
-    7       * 60,
-    8       * 60,
-    8.75    * 60,
-    9       * 60,
-    9.5     * 60,
-];
-
-
-
-// utilities:
-export const convertTimezoneToReadableClock = (timezone: number): string => {
-    const timezoneAbs       = Math.abs(timezone);
-    const timezoneHours     = Math.floor(timezoneAbs / 60);
-    const timezoneMinutes   = Math.round(timezoneAbs - (timezoneHours * 60));
-    return `${(timezone >= 0) ? '+' : '-'}${(timezoneHours < 10) ? '0' : ''}${timezoneHours}:${(timezoneMinutes < 10) ? '0' : ''}${timezoneMinutes}`;
-};
+import {
+    possibleTimezoneValues,
+}                           from './types'
+import {
+    convertTimezoneToReadableClock,
+}                           from './utilities'
 
 
 
@@ -167,7 +120,7 @@ const TimezoneEditor = <TElement extends Element = HTMLElement>(props: TimezoneE
                 </>
             }
         >
-            {possibleValues.map((timezoneOption) =>
+            {possibleTimezoneValues.map((timezoneOption) =>
                 <ListItem
                     // identifiers:
                     key={timezoneOption}
