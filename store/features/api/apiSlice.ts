d1eb6ff05929fab9cdcb7ed527755e4c4dffe12c
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-12-22 21:35:25 +0700

    updated apiSlice

--- store/features/api/apiSlice.ts
+++ store/features/api/apiSlice.ts
@@ -115,7 +115,6 @@ export interface AuthenticationPaymentDataWithBillingAddress
             |'billingCountry'
         >
 {
-    orderId : string
 }
 export type AuthenticationPaymentData = AuthenticationPaymentDataBasic | AuthenticationPaymentDataWithBillingAddress
 
