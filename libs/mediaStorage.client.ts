d509a9e3e688e1299b35fcfc0f95000964875589
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-01-15 09:18:50 +0700

    updated resolveMediaUrl()

--- libs/mediaStorage.client.ts
+++ libs/mediaStorage.client.ts
@@ -4,6 +4,7 @@ import { buildUrl } from 'cloudinary-build-url'
 
 export const resolveMediaUrl = <TNull extends undefined|never = never>(imageId: string|TNull): string|TNull => {
     if (!imageId) return imageId;
+    if (imageId.startsWith('https://') || imageId.startsWith('http://')) return imageId;
     return buildUrl(imageId, {
         cloud : {
             cloudName: process.env.NEXT_PUBLIC_CLOUDINARY_ENV,
