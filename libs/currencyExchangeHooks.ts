1cf2347bdb2290832c1579d598a36352cf9d9392
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-03-17 11:19:14 +0700

    added currencyExchangeHooks

---
+++ libs/currencyExchangeHooks.ts
@@ -0,0 +1,58 @@
+'use client'
+
+// react:
+import {
+    // hooks:
+    useEffect,
+    useState,
+}                           from 'react'
+
+// reusable-ui core:
+import {
+    // react helper hooks:
+    useMountedFlag,
+}                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
+
+// internals:
+import {
+    convertCustomerCurrencyIfRequired,
+}                           from './currencyExchanges'
+import {
+    useCartState,
+}                           from '@/components/Cart/states/cartState'
+
+
+
+// hooks:
+export const useConvertCustomerCurrencyIfRequired = <TNumber extends number|null|undefined>(amount: TNumber): TNumber|undefined => {
+    // states:
+    const {
+        // accessibilities:
+        preferredCurrency,
+    } = useCartState();
+    
+    
+    
+    const [convertedAmount, setConvertedAmount] = useState<TNumber|undefined>(undefined);
+    const isMounted = useMountedFlag();
+    useEffect(() => {
+        // conditions:
+        if (amount === undefined) return;
+        
+        
+        
+        // actions:
+        (async () => {
+            const convertedAmount = await convertCustomerCurrencyIfRequired(
+                amount,
+                preferredCurrency
+            );
+            if (!isMounted.current) return; // the component was unloaded before awaiting returned => do nothing
+            setConvertedAmount(convertedAmount);
+        })();
+    }, [amount, preferredCurrency]);
+    
+    
+    
+    return convertedAmount;
+}
