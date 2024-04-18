b9545352f83f94e1d0319b3b584c5121c949edb7
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-04-18 10:38:27 +0700

    updated utilities

--- models/checkouts/utilities.ts
+++ models/checkouts/utilities.ts
@@ -6,7 +6,7 @@ import type {
 
 
 
-export const isAuthorizedFundData = (data: AuthorizedFundData|PaidFundData|string|undefined): data is AuthorizedFundData => {
+export const isAuthorizedFundData = (data: AuthorizedFundData|PaidFundData|undefined): data is AuthorizedFundData => {
     return (
         !!data
         &&
@@ -15,7 +15,7 @@ export const isAuthorizedFundData = (data: AuthorizedFundData|PaidFundData|strin
         !('paymentAmount' in data)
     );
 }
-export const isPaidFundData      = (data: AuthorizedFundData|PaidFundData|string|undefined): data is PaidFundData => {
+export const isPaidFundData      = (data: AuthorizedFundData|PaidFundData|undefined): data is PaidFundData => {
     return (
         !!data
         &&
