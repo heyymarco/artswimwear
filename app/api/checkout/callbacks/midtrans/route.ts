27985bc2dcd6f30e651580ccfa184c340f4fad96
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-04-20 17:35:03 +0700

    updated midtrans webhook

--- app/api/checkout/callbacks/midtrans/route.ts
+++ app/api/checkout/callbacks/midtrans/route.ts
@@ -1,7 +1,7 @@
-export async function GET(req: Request, res: Response) {
+export async function POSTs(req: Request, res: Response) {
     console.log('webhook: ', {
         headers: Array.from(req.headers.entries()).map(([key, value]) => ({key, value})),
-        // data: await req.json(),
+        data: await req.json(),
     });
     // Return response connected to readable
     return Response.json({
