cb3428258ba821b0d41f54d536f6af7be6a9092f
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-04-22 09:13:16 +0700

    updated midtrans callback

--- app/api/checkout/callbacks/midtrans/route.ts
+++ app/api/checkout/callbacks/midtrans/route.ts
@@ -3,6 +3,12 @@ import {
     midtransTranslateData,
 }                           from '../../paymentProcessors.midtrans'
 
+// models:
+import {
+    // utilities:
+    isAuthorizedFundData,
+}                           from '@/models'
+
 
 
 const sha512test = async (str: string) => {
@@ -53,31 +59,33 @@ export async function POST(req: Request, res: Response): Promise<Response> {
     
     
     const result = midtransTranslateData(midtransPaymentData);
-    console.log('midtrans webhook: ', result);
-    
-    
-    
-    // console.log('webhook: ', {
-    //     headers: Array.from(req.headers.entries()).map(([key, value]) => ({key, value})),
-    //     data: await req.json(),
-    // });
-    /*
-    {
-        "transaction_time": "2023-11-15 18:45:13",
-        "transaction_status": "settlement",
-        "transaction_id": "513f1f01-c9da-474c-9fc9-d5c64364b709",
-        "status_message": "midtrans payment notification",
-        "status_code": "200",
-        "signature_key": "5d103e36f583c5be27a8c85ee4d348b9924ca2af36f852ef4be3a42a82d86631d93ce279395a799beb8bf4edee9402510531bed20d56efedd94587bc47d78399",
-        "settlement_time": "2023-11-15 22:45:13",
-        "payment_type": "gopay",
-        "order_id": "payment_notif_test_G551313466_ae3953e8-5163-423c-9acf-43c27ded20fe",
-        "merchant_id": "G551313466",
-        "gross_amount": "105000.00",
-        "fraud_status": "accept",
-        "currency": "IDR"
+    switch (result) {
+        case undefined: // Transaction not found.
+            // ignore non_existing_transaction
+            break;
+        case null:      // Transaction creation was denied.
+            // ignore failure_create_transaction
+            break;
+        
+        
+        
+        case false: { // Transaction was deleted due to canceled or expired.  
+            // TODO: delete draftOrder from DB
+            break;
+        }
+        
+        
+        
+        default: {
+            // AuthorizedFundData : Authorized for payment.
+            // PaidFundData       : Paid.
+            if (isAuthorizedFundData(result)) {
+                // ignore Authorized for payment
+                break;
+            } // if
+        }
     }
-    */
+    console.log('midtrans webhook: ', result);
     
     
     
