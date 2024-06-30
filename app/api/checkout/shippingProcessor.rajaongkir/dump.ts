5ddd0c98b31ad0c1617c563645977d3a8c66f0f5
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-07-01 03:16:42 +0700

    updated rajaongkir

---
+++ app/api/checkout/shippingProcessor.rajaongkir/dump.ts
@@ -0,0 +1,87 @@
+import {
+    stateCityToIdMap,
+} from './stateCityToIdMap'
+
+import {
+    State,
+    City,
+} from 'country-state-city'
+
+
+
+const originId   = stateCityToIdMap.get('DI Yogyakarta/Yogyakarta'.trim().toLowerCase()) ?? (() => { throw 'Error'; })();
+const weightGram = 1000;
+const courier    = 'jne';
+
+
+
+const kalimantanBarat = [
+    { name: 'Kabupaten Bengkayang' },
+    { name: 'Kabupaten Kapuas Hulu' },
+    { name: 'Kabupaten Kayong Utara' },
+    { name: 'Kabupaten Ketapang' },
+    { name: 'Kabupaten Kubu Raya' },
+    { name: 'Kabupaten Landak' },
+    { name: 'Kabupaten Melawi' },
+    { name: 'Kabupaten Mempawah' },
+    { name: 'Kabupaten Sambas' },
+    { name: 'Kabupaten Sanggau' },
+    { name: 'Kabupaten Sekadau' },
+    { name: 'Kabupaten Sintang' },
+    { name: 'Kota Pontianak' },
+    { name: 'Kota Singkawang' },
+];
+
+
+
+let counter = 0;
+for (const { name: stateRaw, isoCode } of (State.getStatesOfCountry('ID') ?? [])) {
+    const state = stateRaw.trim().toLowerCase();
+    for (const { name: cityRaw } of ((stateRaw !== 'Kalimantan Barat') ? (City.getCitiesOfState('ID', isoCode)) : kalimantanBarat)) {
+        const city = cityRaw.trim().toLowerCase();
+        const destinationId = stateCityToIdMap.get(`${state}/${city}`.trim().toLowerCase())
+        if (destinationId === undefined) {
+            // console.log(`['${stateRaw}/${cityRaw}', 'ERROR']`);
+            continue;
+        } // if
+        
+        
+        
+        try {
+            const res = await fetch('https://api.rajaongkir.com/starter/cost', {
+                method  : 'POST',
+                headers : {
+                    'key': 'd1ed66bcc966b31be08289752c1b1bbf',
+                    'Content-Type': 'application/json',
+                },
+                body    : JSON.stringify({
+                    origin      : originId,
+                    destination : destinationId,
+                    weight      : weightGram,
+                    courier     : courier,
+                }),
+            });
+            const json = await res.json();
+            const {
+                rajaongkir : {
+                    results : [
+                        {
+                            costs : costsRaw,
+                        },
+                    ],
+                },
+            } = json;
+            const costs = costsRaw.map((cost: any) => [cost.service, cost.cost]);
+            console.log((++counter), `['${stateRaw}/${cityRaw}', ${JSON.stringify(costs)}],`);
+        }
+        catch (error: any) {
+            console.log((++counter), `['${stateRaw}/${cityRaw}', 'ERROR', ${error}]`);
+        } // try
+        
+        
+        
+        await new Promise<void>((resolve) => {
+            setTimeout(resolve, (10 + (Math.random() * 30)) * 1000);
+        });
+    } // for
+} // for
