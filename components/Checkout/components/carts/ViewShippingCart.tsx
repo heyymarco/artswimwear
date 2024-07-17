554c3518322908b21c783e25560663d116be0ac5
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-07-18 04:53:15 +0700

    updated comment

--- components/Checkout/components/carts/ViewShippingCart.tsx
+++ components/Checkout/components/carts/ViewShippingCart.tsx
@@ -37,7 +37,7 @@ const ViewShippingCart = (): JSX.Element|null => {
     // jsx:
     if (!isCheckoutReady)           return null;
     if (totalShippingCost === null) return null; // not_physical_product => nothing to display
-    if ((totalShippingCost === undefined) && (shippingProvider !== undefined)) return null; // unknown_kind_product -and- has shippingProvider selected => something error => nothing to display
+    if ((totalShippingCost === undefined) && (shippingProvider !== undefined)) return null; // unknown_kind_product (due to incomplete loading of related data) -and- has shippingProvider selected => something error => nothing to display
     return (
         <p className='currencyBlock'>
             <span>Shipping</span>
