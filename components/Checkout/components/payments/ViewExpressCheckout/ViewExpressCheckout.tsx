f3ce2ee242f0aea44e81e96e1cdc4ddbb8ef80d8
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-07-28 20:39:59 +0700

    updated ViewExpressCheckout

--- components/Checkout/components/payments/ViewExpressCheckout/ViewExpressCheckout.tsx
+++ components/Checkout/components/payments/ViewExpressCheckout/ViewExpressCheckout.tsx
@@ -498,9 +498,9 @@ const ViewExpressCheckout = (props: ViewExpressCheckoutProps): JSX.Element|null 
             throw Error('Oops, an error occured!');
         } // try
     });
-    const handleRevertDraftOrder         = useEvent((orderId: string): void => {
+    const handleRevertDraftOrder         = useEvent((rawOrderId: string): void => {
         // notify to cancel transaction, so the draftOrder (if any) will be reverted:
-        doMakePayment(orderId, /*paid:*/false, { cancelOrder: true })
+        doMakePayment(rawOrderId, /*paid:*/false, { cancelOrder: true })
         .catch(() => {
             // ignore any error
         });
