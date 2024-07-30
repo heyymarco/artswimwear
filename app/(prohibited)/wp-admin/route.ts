76f7afb90fdf51fac58a0907912eb33236420a7b
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-07-31 02:34:05 +0700

    added wp-admin blocker

---
+++ app/(prohibited)/wp-admin/route.ts
@@ -0,0 +1,20 @@
+// configs:
+export const dynamic = 'force-static';
+
+
+
+// routers:
+const handler = (req: Request) => {
+    return new Response(undefined, {
+        status : 404,
+    });
+}
+export {
+    handler as GET,
+    handler as POST,
+    handler as PUT,
+    handler as PATCH,
+    handler as DELETE,
+    handler as HEAD,
+    handler as OPTIONS,
+}
