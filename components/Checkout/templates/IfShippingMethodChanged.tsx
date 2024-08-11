0e066e7ec9c40fc18854557c87917994f82efebf
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-08-11 16:43:54 +0700

    added template: IfShippingMethodChanged & IfNotShippingMethodChanged

---
+++ components/Checkout/templates/IfShippingMethodChanged.tsx
@@ -0,0 +1,51 @@
+// react:
+import {
+    // react:
+    default as React,
+}                           from 'react'
+
+// internals:
+import {
+    // hooks:
+    useShippingContext,
+}                           from './shippingDataContext'
+import {
+    // hooks:
+    useOrderDataContext,
+}                           from './orderDataContext'
+
+
+
+// react components:
+export const IfShippingMethodChanged = (props: React.PropsWithChildren<{}>): React.ReactNode => {
+    // contexts:
+    const {
+        // data:
+        prevShippingTracking,
+    } = useShippingContext();
+    
+    const {
+        // data:
+        order : {
+            shippingAddress,
+        },
+        shippingTracking,
+    } = useOrderDataContext();
+    
+    
+    
+    // jsx:
+    if (!shippingAddress) return null; // no shipping address => non_physical_product => ignore
+    if (!shippingTracking) return null; // not already shipped => ignore
+    
+    const isShippingMethodChanged : boolean = (
+        (prevShippingTracking !== undefined) // do not detect changes if `prevShippingTracking` not provider for comparison
+        &&
+        (prevShippingTracking.shippingCarrier?.trim().toLowerCase() !== shippingTracking.shippingCarrier?.trim().toLowerCase()) // detect shipping carrier name changes
+        &&
+        (prevShippingTracking.shippingNumber?.trim() !== shippingTracking.shippingNumber?.trim()) // detect shipping tracking number changes
+    );
+    
+    if (!isShippingMethodChanged) return null; // not changed => ignore
+    return props.children;
+};
