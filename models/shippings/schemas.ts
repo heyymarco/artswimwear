73db3a6b3f266188793d554e170f25a6a653d985
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-08-23 11:21:24 +0700

    added zod

---
+++ models/shippings/schemas.ts
@@ -0,0 +1,32 @@
+// models:
+import {
+    type ShippingAddressDetail,
+    type BillingAddressDetail,
+}                           from './types'
+
+// zod:
+import {
+    z,
+}                           from 'zod'
+
+
+
+// schemas:
+export const countrySchema = z.enum([
+    'AF', 'AX', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR', 'AM', 'AW', 'AU', 'AT', 'AZ', 'BS', 'BH', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BA', 'BW', 'BR', 'IO', 'BN', 'BG', 'BF', 'BI', 'KH', 'CM', 'CA', 'CV', 'KY', 'CF', 'TD', 'CL', 'CN', 'CX', 'CC', 'CO', 'KM', 'CG', 'CD', 'CK', 'CR', 'CI', 'HR', 'CU', 'CY', 'CZ', 'DK', 'DJ', 'DM', 'DO', 'EC', 'EG', 'SV', 'GQ', 'ER', 'EE', 'ET', 'FK', 'FO', 'FJ', 'FI', 'FR', 'GF', 'PF', 'GA', 'GM', 'GE', 'DE', 'GH', 'GI', 'GR', 'GL', 'GD', 'GP', 'GU', 'GT', 'GG', 'GN', 'GW', 'GY', 'HT', 'VA', 'HN', 'HK', 'HU', 'IS', 'IN', 'ID', 'IR', 'IQ', 'IE', 'IM', 'IL', 'IT', 'JM', 'JP', 'JE', 'JO', 'KZ', 'KE', 'KI', 'KP', 'KR', 'KW', 'KG', 'LA', 'LV', 'LB', 'LS', 'LR', 'LY', 'LI', 'LT', 'LU', 'MO', 'MK', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT', 'MH', 'MQ', 'MR', 'MU', 'YT', 'MX', 'FM', 'MD', 'MC', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM', 'NA', 'NR', 'NP', 'NL', 'AN', 'NC', 'NZ', 'NI', 'NE', 'NG', 'NU', 'NF', 'MP', 'NO', 'OM', 'PK', 'PW', 'PS', 'PA', 'PG', 'PY', 'PE', 'PH', 'PN', 'PL', 'PT', 'PR', 'QA', 'RO', 'RU', 'RW', 'RE', 'BL', 'SH', 'KN', 'LC', 'MF', 'PM', 'VC', 'WS', 'SM', 'ST', 'SA', 'SN', 'RS', 'SC', 'SL', 'SG', 'SK', 'SI', 'SB', 'SO', 'ZA', 'SS', 'GS', 'ES', 'LK', 'SD', 'SR', 'SJ', 'SZ', 'SE', 'CH', 'SY', 'TW', 'TJ', 'TZ', 'TH', 'TL', 'TG', 'TK', 'TO', 'TT', 'TN', 'TR', 'TM', 'TC', 'TV', 'UG', 'UA', 'AE', 'GB', 'US', 'UY', 'UZ', 'VU', 'VE', 'VN', 'VG', 'VI', 'WF', 'YE', 'ZM', 'ZW',
+]);
+export const BaseAddressDetailSchema = z.object({
+    // data:
+    country   : countrySchema,
+    state     : z.string().trim().min(2).max(50),
+    city      : z.string().trim().min(2).max(50),
+    zip       : z.string().trim().min(2).max(11).nullable(),
+    address   : z.string().trim().min(5).max(90),
+    
+    firstName : z.string().trim().min(2).max(30),
+    lastName  : z.string().trim().min(1).max(30),
+    phone     : z.string().trim().min(5).max(15),
+}) satisfies z.Schema<ShippingAddressDetail & BillingAddressDetail>;
+
+export const ShippingAddressDetailSchema = BaseAddressDetailSchema;
+export const BillingAddressDetailSchema  = BaseAddressDetailSchema;
