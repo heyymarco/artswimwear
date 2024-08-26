3675b440a4ac2ba98a452eb5d24e4154c49b6387
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-08-26 17:36:49 +0700

    updated cart api

--- app/api/(protected)/cart/route.ts
+++ app/api/(protected)/cart/route.ts
@@ -1,9 +1,3 @@
-// next-js:
-import {
-    NextRequest,
-    NextResponse,
-}                           from 'next/server'
-
 // next-auth:
 import {
     getServerSession,
@@ -22,6 +16,10 @@ import {
 // models:
 import {
     type CartDetail,
+    
+    
+    
+    CartDetailSchema,
     
     
     
@@ -52,8 +50,8 @@ interface RequestContext {
         /* no params yet */
     }
 }
-const router  = createEdgeRouter<NextRequest, RequestContext>();
-const handler = async (req: NextRequest, ctx: RequestContext) => router.run(req, ctx) as Promise<any>;
+const router  = createEdgeRouter<Request, RequestContext>();
+const handler = async (req: Request, ctx: RequestContext) => router.run(req, ctx) as Promise<any>;
 export {
     handler as GET,
     handler as POST,
@@ -67,7 +65,7 @@ router
 .use(async (req, ctx, next) => {
     // conditions:
     const session = await getServerSession(authOptions);
-    if (!session) return NextResponse.json({ error: 'Please sign in.' }, { status: 401 }); // handled with error: unauthorized
+    if (!session) return Response.json({ error: 'Please sign in.' }, { status: 401 }); // handled with error: unauthorized
     (req as any).session = session;
     
     
@@ -80,7 +78,7 @@ router
     const session = (req as any).session as Session;
     const customerId = session.user?.id;
     if (!customerId) {
-        return NextResponse.json({ error: 'Please sign in.' }, { status: 401 }); // handled with error: unauthorized
+        return Response.json({ error: 'Please sign in.' }, { status: 401 }); // handled with error: unauthorized
     } // if
     //#endregion validating privileges
     
@@ -97,11 +95,187 @@ router
         
         
         
-        return NextResponse.json(cartDetail); // handled with success
+        return Response.json(cartDetail); // handled with success
     }
     catch (error: any) {
         console.log('ERROR: ', error);
-        return NextResponse.json({ error: error }, { status: 500 }); // handled with error
+        return Response.json({ error: error }, { status: 500 }); // handled with error
     } // try
     //#endregion query result
+})
+.patch(async (req) => {
+    //#region parsing and validating request
+    const requestData = await (async () => {
+        try {
+            const data = await req.json();
+            return {
+                cartDetail : CartDetailSchema.parse(data),
+            };
+        }
+        catch {
+            return null;
+        } // try
+    })();
+    if (requestData === null) {
+        return Response.json({
+            error: 'Invalid data.',
+        }, { status: 400 }); // handled with error
+    } // if
+    const {
+        cartDetail : {
+            items,
+            checkout,
+            ...cartDetail
+        },
+    } = requestData;
+    //#endregion parsing and validating request
+    
+    
+    
+    //#region validating privileges
+    const session = (req as any).session as Session;
+    const customerId = session.user?.id;
+    if (!customerId) {
+        return Response.json({ error: 'Please sign in.' }, { status: 401 }); // handled with error: unauthorized
+    } // if
+    //#endregion validating privileges
+    
+    
+    
+    //#region save changes
+    await (
+        !items.length
+        ? prisma.cart.deleteMany({
+            where  : {
+                // relations:
+                parentId : customerId, // unique, guarantees only delete one or zero
+            },
+        })
+        : prisma.$transaction(async (prismaTransaction) => {
+            const oldData = await prismaTransaction.cart.findUnique({
+                where  : {
+                    // relations:
+                    parentId : customerId,
+                },
+                select : {
+                    checkout : {
+                        select : {
+                            id              : true,
+                            shippingAddress : {
+                                select : {
+                                    id : true,
+                                },
+                            },
+                            billingAddress  : {
+                                select : {
+                                    id : true,
+                                },
+                            },
+                        },
+                    },
+                },
+            });
+            const hasCheckout        = oldData?.checkout?.id                  !== undefined;
+            const hasShippingAddress = oldData?.checkout?.shippingAddress?.id !== undefined;
+            const hasBillingAddress  = oldData?.checkout?.billingAddress?.id  !== undefined;
+            
+            
+            
+            return prisma.cart.upsert({
+                where  : {
+                    // relations:
+                    parentId : customerId,
+                },
+                update : {
+                    // data:
+                    ...cartDetail,
+                    checkout : { // compound_like relation
+                        // nested_delete if set to null:
+                        delete : ((checkout !== null) /* do NOT delete if NOT null */ || !hasCheckout /* do NOT delete if NOTHING to delete */) ? undefined : {
+                            // do DELETE
+                            // no condition needed because one to one relation
+                        },
+                        
+                        // two_conditional nested_update:
+                        upsert : ((checkout === null) /* do NOT update if null */) ? undefined : {
+                            update : { // prefer   to `update` if already exist
+                                ...checkout,
+                                shippingAddress : { // compound_like relation
+                                    // nested_delete if set to null:
+                                    delete : ((checkout.shippingAddress !== null) /* do NOT delete if NOT null */ || !hasShippingAddress /* do NOT delete if NOTHING to delete */) ? undefined : {
+                                        // do DELETE
+                                        // no condition needed because one to one relation
+                                    },
+                                    
+                                    // two_conditional nested_update:
+                                    upsert : ((checkout.shippingAddress === null) /* do NOT update if null */) ? undefined : {
+                                        update : checkout.shippingAddress, // prefer   to `update` if already exist
+                                        create : checkout.shippingAddress, // fallback to `create` if not     exist
+                                    },
+                                },
+                                billingAddress  : { // compound_like relation
+                                    // nested_delete if set to null:
+                                    delete : ((checkout.billingAddress !== null) /* do NOT delete if NOT null */ || !hasBillingAddress /* do NOT delete if NOTHING to delete */) ? undefined : {
+                                        // do DELETE
+                                        // no condition needed because one to one relation
+                                    },
+                                    
+                                    // two_conditional nested_update:
+                                    upsert : ((checkout.billingAddress === null) /* do NOT update if null */) ? undefined : {
+                                        update : checkout.billingAddress, // prefer   to `update` if already exist
+                                        create : checkout.billingAddress, // fallback to `create` if not     exist
+                                    },
+                                },
+                            },
+                            create : { // fallback to `create` if not     exist
+                                ...checkout,
+                                shippingAddress : (checkout.shippingAddress === null) ? undefined : { // compound_like relation
+                                    // one_conditional nested_update:
+                                    create : checkout.shippingAddress,
+                                },
+                                billingAddress  : (checkout.billingAddress  === null) ? undefined : { // compound_like relation
+                                    // one_conditional nested_update:
+                                    create : checkout.billingAddress,
+                                },
+                            },
+                        },
+                    },
+                    items : { // array_like relation
+                        // clear the existing item(s), if any:
+                        deleteMany : {},
+                        
+                        // create all item(s):
+                        create : items, // the `items` is guaranteed not_empty because of the conditional `!items.length`
+                    },
+                },
+                create : {
+                    // data:
+                    ...cartDetail,
+                    checkout : (checkout === null) ? undefined : { // compound_like relation
+                        create : {
+                            ...checkout,
+                            shippingAddress : (checkout.shippingAddress === null) ? undefined : { // compound_like relation
+                                // one_conditional nested_update:
+                                create : checkout.shippingAddress,
+                            },
+                            billingAddress  : (checkout.billingAddress  === null) ? undefined : { // compound_like relation
+                                // one_conditional nested_update:
+                                create : checkout.billingAddress,
+                            },
+                        },
+                    },
+                    items : { // array_like relation
+                        // one_conditional nested_update:
+                        create : items, // the `items` is guaranteed not_empty because of the conditional `!items.length`
+                    },
+                    
+                    
+                    
+                    // relations:
+                    parentId : customerId,
+                },
+            })
+        })
+    );
+    //#endregion save changes
 });
