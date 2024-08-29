607f94617188cd121aa5f4ad8ce7bf39b7469d12
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-08-29 08:40:22 +0700

    updated checkout config

--- checkout.config.shared.tsx
+++ checkout.config.shared.tsx
@@ -43,7 +43,6 @@ export const checkoutConfigShared   : CheckoutConfigShared = {
                 enabled             : true,
                 supportedCurrencies : [
                     'IDR',
-                    'USD',
                 ],
             },
             paypal                  : {
