5739dc69a5a1e5b5bd164c271106b7f8f90bf1cd
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-09-05 00:22:46 +0700

    added customer schemas

---
+++ models/customers/schemas.ts
@@ -0,0 +1,33 @@
+// zod:
+import {
+    z,
+}                           from 'zod'
+
+// models:
+import {
+    type CustomerDetail,
+    type GuestDetail,
+}                           from './types'
+import {
+    HumanFullNameSchema,
+    EmailSchema,
+    ImageUrlSchema,
+}                           from '../commons'
+
+
+
+// schemas:
+export const CustomerDetailSchema = z.object({
+    // data:
+    name     : HumanFullNameSchema,
+    email    : EmailSchema,
+    image    : ImageUrlSchema,
+}) satisfies z.Schema<Omit<CustomerDetail, 'id'|'username'>>;
+
+
+
+export const GuestDetailSchema = z.object({
+    // data:
+    name     : HumanFullNameSchema,
+    email    : EmailSchema,
+}) satisfies z.Schema<Omit<GuestDetail, 'id'>>;
