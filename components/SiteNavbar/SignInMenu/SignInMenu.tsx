237d7a54d8f89a8810636499126458728718414c
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-09-06 08:39:45 +0700

    updated SignInMenu

--- components/SiteNavbar/SignInMenu/SignInMenu.tsx
+++ components/SiteNavbar/SignInMenu/SignInMenu.tsx
@@ -10,6 +10,7 @@ import {
     // hooks:
     useState,
     useRef,
+    useEffect,
 }                           from 'react'
 
 // next-js:
@@ -120,13 +121,27 @@ const SignInMenu = (props: SignInMenuProps): JSX.Element|null => {
     
     // sessions:
     const { data: session, status: sessionStatus } = useSession();
-    const isSignedIn  = (sessionStatus === 'authenticated');
-    const isSignedOut = (sessionStatus === 'unauthenticated');
-    const isBusy      = (sessionStatus === 'loading');
+    const [isLoading, setIsLoading] = useState<boolean>(sessionStatus === 'loading'); // the `sessionStatus === 'loading'` is not quite reliable, so we use additional loading state
+    const isSignedIn  = !isLoading && (sessionStatus === 'authenticated');
+    const isSignedOut = !isLoading && (sessionStatus === 'unauthenticated');
+    const isBusy      =  isLoading || (sessionStatus === 'loading');
     const { name: customerName, email: customerEmail, image: customerImage } = session?.user ?? {};
     const customerNameParts = customerName?.split(/\s+/gi);
     const customerFirstName = customerNameParts?.[0];
     const customerShortRestName = !!customerNameParts && (customerNameParts.length >= 2) ? customerNameParts[customerNameParts.length - 1][0] : undefined;
+    
+    
+    
+    // effects:
+    useEffect(() => {
+        // conditions:
+        if (sessionStatus === 'loading') return; // only interested to FULLY signedIn|signedOut
+        
+        
+        
+        // actions:
+        setIsLoading(false); // reset
+    }, [sessionStatus]);
     
     
     
@@ -201,6 +216,7 @@ const SignInMenu = (props: SignInMenuProps): JSX.Element|null => {
                             break;
                         
                         case 'signOut':
+                            setIsLoading(true); // the `sessionStatus === 'loading'` is not quite reliable, so we use additional loading state
                             signOut({ redirect: false, callbackUrl: pathname }); // when signed in back, redirects to current url
                             break;
                     } // switch
