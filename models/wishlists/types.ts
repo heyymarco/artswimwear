6ea17eee9330e3e022bc1cfb51541f367f064f01
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-09-10 23:30:27 +0700

    added wishlist models

---
+++ models/wishlists/types.ts
@@ -0,0 +1,96 @@
+// models:
+import {
+    type WishlistGroup,
+    type Wishlist,
+}                           from '@prisma/client'
+
+
+
+export interface WishlistGroupDetail
+    extends
+        WishlistGroup
+{
+}
+
+
+
+export interface WishlistDetail
+    extends
+        Omit<Wishlist,
+            // records:
+            |'id'
+            |'updatedAt'
+            
+            
+            
+            // relations:
+            |'parentId'
+            |'groupId'
+        >
+{
+}
+
+
+
+export interface CreateWishlistGroupRequest
+    extends
+        Pick<WishlistGroup,
+            // data:
+            |'name'
+        >
+{
+}
+export interface UpdateWishlistGroupRequest
+    extends
+        Pick<WishlistGroup,
+            // records:
+            |'id'
+            
+            
+            
+            // data:
+            |'name'
+        >
+{
+}
+export interface DeleteWishlistGroupRequest
+    extends
+        Pick<WishlistGroup,
+            // records:
+            |'id'
+        >
+{
+}
+
+
+
+export interface GetWishlistRequest
+    extends
+        Pick<Wishlist,
+            // relations:
+            |'groupId'
+        >
+{
+}
+
+export interface CreateOrUpdateWishlistRequest
+    extends
+        Pick<Wishlist,
+            // relations:
+            |'productId'
+        >,
+        Partial<Pick<Wishlist,
+            // relations:
+            |'groupId'
+        >>
+{
+}
+
+export interface DeleteWishlistRequest
+    extends
+        Pick<Wishlist,
+            // relations:
+            |'productId'
+        >
+{
+}
