e54c8a0fedd5dc4030988d9ce783de3c8e582d58
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-10-26 05:54:42 +0700

    updated GetWishPageRequestSchema

--- models/wishes/schemas.ts
+++ models/wishes/schemas.ts
@@ -37,7 +37,7 @@ export const DeleteWishGroupRequestSchema = z.object({
 export const GetWishPageRequestSchema = PaginationArgSchema.merge(
     z.object({
         groupId : ModelIdSchema/*.nullable()*/.optional(), // remove support for `null`; `string`: get grouped wishes, `undefined`: get all wishes (grouped + ungrouped), `null`: get ungrouped wishes
-    })
+    }) satisfies z.Schema<{ groupId?: string }>
 ) satisfies z.Schema<GetWishPageRequest>;
 
 export const CreateOrUpdateWishRequestSchema = z.object({
