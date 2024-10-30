cee56da697bda17224e13768c024c607742b3cc7
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-10-31 01:30:44 +0700

    updated RouterUpdater

--- components/explorers/CategoryExplorer/RouterUpdater.tsx
+++ components/explorers/CategoryExplorer/RouterUpdater.tsx
@@ -11,6 +11,9 @@ import {
     useRef,
     useEffect,
 }                           from 'react'
+import {
+    useImmer,
+}                           from 'use-immer'
 
 // next-js:
 import {
@@ -49,31 +52,31 @@ const RouterUpdater = (): JSX.Element|null => {
     
     // effects:
     // sync the pathname to the path of selected category:
-    const pathname                = usePathname();
-    const router                  = useRouter();
-    const prevParentCategoriesRef = useRef<CategoryParentInfo[]|undefined>(undefined /* initially unsynced */);
+    const pathname                      = usePathname();
+    const router                        = useRouter();
+    const prevParentCategoriesRef       = useRef<CategoryParentInfo[]|undefined>(undefined /* initially unsynced */);
+    const [historyUrls, setHistoryUrls] = useImmer<string[]>(() => [pathname]);
     useEffect(() => {
         // conditions:
         if (prevParentCategoriesRef.current === parentCategories) return; // already the same => ignore
-        const prevParentCategories = prevParentCategoriesRef.current;
         prevParentCategoriesRef.current = parentCategories;               // sync
-        if (!parentCategories.length) return; // the root category is not yet selected or loaded => ignore
         
         
         
         // actions:
-        const oldPathname = !prevParentCategories ? undefined : `${categoriesPath}/${prevParentCategories.map(({category: {path}}) => path).join('/')}`;
-        const newPathname = `${categoriesPath}/${parentCategories.map(({category: {path}}) => path).join('/')}`;
+        const newPathname = `${categoriesPath}${!parentCategories.length ? '' : `/${parentCategories.map(({category: {path}}) => path).join('/')}`}`;
         if (newPathname.toLowerCase() !== pathname.toLowerCase()) {
-            if ((prevParentCategories?.length === (parentCategories.length + 1)) && ((): boolean => { // if a back action detected
-                for (let index = 0, maxIndex = parentCategories.length - 1; index <= maxIndex; index++) {
-                    if (parentCategories[index] !== prevParentCategories[index]) return false;
-                } // for
-                return (pathname.toLowerCase() === oldPathname?.toLowerCase());
-            })()) {
+            const backPathname = (historyUrls.length < 2) ? undefined : historyUrls.at(-2);
+            if (newPathname === backPathname) { // if a back action detected
+                setHistoryUrls((historyUrls) => {
+                    historyUrls.pop();
+                });
                 router.back();
             }
             else {
+                setHistoryUrls((historyUrls) => {
+                    historyUrls.push(newPathname);
+                });
                 router.push(newPathname, { scroll: false }); // intercept the url
             } // if
         } // if
