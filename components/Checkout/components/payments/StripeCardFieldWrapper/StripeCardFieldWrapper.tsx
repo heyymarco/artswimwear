d843425cb0a22c7294b4911498c349e590327c89
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-11-19 13:00:24 +0700

    updated StripeCardFieldWrapper

--- components/Checkout/components/payments/StripeCardFieldWrapper/StripeCardFieldWrapper.tsx
+++ components/Checkout/components/payments/StripeCardFieldWrapper/StripeCardFieldWrapper.tsx
@@ -14,16 +14,6 @@ import {
 
 // reusable-ui core:
 import {
-    // a color management system:
-    colorValues,
-    
-    
-    
-    // a typography management system:
-    typoValues,
-    
-    
-    
     // react helper hooks:
     useEvent,
 }                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
@@ -33,11 +23,6 @@ import {
     // base-components:
     EditableTextControlProps,
     EditableTextControl,
-    
-    
-    
-    // simple-components:
-    inputValues,
 }                           from '@reusable-ui/components'      // a set of official Reusable-UI components
 
 // styles:
@@ -58,47 +43,10 @@ import {
     type CardCvcElementProps,
 }                           from '@stripe/react-stripe-js'
 
-
-
-// styles:
-const style : StripeElementStyle = {
-    base : {
-        fontSize            : typoValues.fontSizeMd?.toString(),
-        fontFamily          : typoValues.fontFamilySansSerief?.toString(),
-        fontWeight          : typoValues.fontWeightNormal?.toString(),
-        fontStyle           : typoValues.fontStyle?.toString(),
-        textDecoration      : typoValues.textDecoration?.toString(),
-        lineHeight          : typoValues.lineHeightMd?.toString(),
-        
-        color               : colorValues.primaryBold.toString(),
-        
-        '::selection' : {
-            color           : colorValues.primaryText.toString(),
-            backgroundColor : colorValues.primary.toString(),
-        },
-    },
-    empty : {
-        '::placeholder' : {
-            // color           : `color-mix(in srgb, currentColor, ${(inputValues.placeholderOpacity as number) * 100}% transparent)`, // doesn't work
-            // opacity         : inputValues.placeholderOpacity as number, // doesn't work
-            color           : colorValues.primaryBold.alpha(inputValues.placeholderOpacity as number).toString(),
-        },
-    },
-    complete : {
-        color               : colorValues.successBold.toString(),
-        '::selection' : {
-            color           : colorValues.successText.toString(),
-            backgroundColor : colorValues.success.toString(),
-        },
-    },
-    invalid : {
-        color               : colorValues.dangerBold.toString(),
-        '::selection' : {
-            color           : colorValues.dangerText.toString(),
-            backgroundColor : colorValues.danger.toString(),
-        },
-    },
-};
+// internals:
+import {
+    stripeCardFieldsStyle,
+}                           from './styles'
 
 
 
@@ -155,7 +103,7 @@ const StripeCardFieldWrapper = (props: StripeCardFieldWrapperProps) => {
             {
                 // options:
                 options : {
-                    style : style,
+                    style : stripeCardFieldsStyle,
                 },
                 
                 
---
+++ components/Checkout/components/payments/StripeCardFieldWrapper/styles.ts
@@ -0,0 +1,63 @@
+// reusable-ui core:
+import {
+    // a color management system:
+    colorValues,
+    
+    
+    
+    // a typography management system:
+    typoValues,
+}                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
+
+// reusable-ui components:
+import {
+    // simple-components:
+    inputValues,
+}                           from '@reusable-ui/components'      // a set of official Reusable-UI components
+
+// stripe:
+import {
+    type StripeElementStyle,
+}                           from '@stripe/stripe-js'
+
+
+
+// styles:
+export const stripeCardFieldsStyle : StripeElementStyle = {
+    base : {
+        fontSize            : typoValues.fontSizeMd?.toString(),
+        fontFamily          : typoValues.fontFamilySansSerief?.toString(),
+        fontWeight          : typoValues.fontWeightNormal?.toString(),
+        fontStyle           : typoValues.fontStyle?.toString(),
+        textDecoration      : typoValues.textDecoration?.toString(),
+        lineHeight          : typoValues.lineHeightMd?.toString(),
+        
+        color               : colorValues.primaryBold.toString(),
+        
+        '::selection' : {
+            color           : colorValues.primaryText.toString(),
+            backgroundColor : colorValues.primary.toString(),
+        },
+    },
+    empty : {
+        '::placeholder' : {
+            // color           : `color-mix(in srgb, currentColor, ${(inputValues.placeholderOpacity as number) * 100}% transparent)`, // doesn't work
+            // opacity         : inputValues.placeholderOpacity as number, // doesn't work
+            color           : colorValues.primaryBold.alpha(inputValues.placeholderOpacity as number).toString(),
+        },
+    },
+    complete : {
+        color               : colorValues.successBold.toString(),
+        '::selection' : {
+            color           : colorValues.successText.toString(),
+            backgroundColor : colorValues.success.toString(),
+        },
+    },
+    invalid : {
+        color               : colorValues.dangerBold.toString(),
+        '::selection' : {
+            color           : colorValues.dangerText.toString(),
+            backgroundColor : colorValues.danger.toString(),
+        },
+    },
+};
