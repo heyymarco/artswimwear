e573bc9859c6cb07c4a8d44d1ac1471c621171fe
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-12-22 14:52:28 +0700

    decoupled sample-capturePaymentMethod.js

--- libs/payments/processors/paypal/index.ts
+++ libs/payments/processors/paypal/index.ts
@@ -1005,34 +1005,8 @@ export const paypalCapturePaymentMethod = async (vaultToken: string): Promise<Pa
     });
     const paypalPaymentData = await paypalHandleResponse(response);
     /*
-        {
-            id: "7tc409525d432030y",
-            customer: {
-                id: "VecXibSgbC",
-            },
-            payment_source: {
-                card: {
-                    name: "John Doe",
-                    last_digits: "6961",
-                    brand: "AMEX",
-                    expiry: "2025-01",
-                },
-            },
-            links: [
-                {
-                    href: "https://api.sandbox.paypal.com/v3/vault/payment-tokens/7tc409525d432030y",
-                    rel: "self",
-                    method: "GET",
-                    encType: "application/json",
-                },
-                {
-                    href: "https://api.sandbox.paypal.com/v3/vault/payment-tokens/7tc409525d432030y",
-                    rel: "delete",
-                    method: "DELETE",
-                    encType: "application/json",
-                },
-            ],
-        }
+        sample responses:
+        './sample-responses/sample-capturePaymentMethod.js'
     */
     const {
         id : paymentMethodProviderId,
---
+++ libs/payments/processors/paypal/sample-responses/sample-capturePaymentMethod.js
@@ -0,0 +1,28 @@
+const sample = {
+    id: "7tc409525d432030y",
+    customer: {
+        id: "VecXibSgbC",
+    },
+    payment_source: {
+        card: {
+            name: "John Doe",
+            last_digits: "6961",
+            brand: "AMEX",
+            expiry: "2025-01",
+        },
+    },
+    links: [
+        {
+            href: "https://api.sandbox.paypal.com/v3/vault/payment-tokens/7tc409525d432030y",
+            rel: "self",
+            method: "GET",
+            encType: "application/json",
+        },
+        {
+            href: "https://api.sandbox.paypal.com/v3/vault/payment-tokens/7tc409525d432030y",
+            rel: "delete",
+            method: "DELETE",
+            encType: "application/json",
+        },
+    ],
+}
