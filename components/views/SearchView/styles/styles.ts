0bcee3b76cc24d5a25b05ecbd133e66380d559e4
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2025-02-16 07:12:08 +0700

    updated SearchView styles

--- components/views/SearchView/styles/styles.ts
+++ components/views/SearchView/styles/styles.ts
@@ -1,6 +1,7 @@
 // cssfn:
 import {
     // writes css in javascript:
+    rule,
     children,
     style,
     scope,
@@ -12,13 +13,37 @@ import {
     spacers,
 }                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
 
+// reusable-ui components:
+import {
+    // base-content-components:
+    containers,
+    
+    
+    
+    // simple components:
+    usesIcon,
+}                           from '@reusable-ui/components'      // a set of official Reusable-UI components
+
 
 
 // styles:
 const usesMainLayout = () => {
+    // dependencies:
+    
+    // features:
+    const {iconVars} = usesIcon();
+    
+    
+    
     return style({
         // layouts:
         display: 'grid',
+        gridTemplate: [[
+            '"search " min-content',
+            '"results" 1fr',
+            '/',
+            '1fr',
+        ]],
         
         
         
@@ -28,8 +53,40 @@ const usesMainLayout = () => {
         
         
         // children:
-        ...children('form', {
+        ...children('.search', {
+            gridArea: 'search',
+            
+            
+            
+            // layouts:
             display: 'grid',
+        }),
+        ...children('.results', {
+            gridArea: 'results',
+            
+            ...rule('.empty', {
+                ...children('.body', {
+                    ...children('[role="list"]', {
+                        justifyContent : 'center',
+                        alignContent   : 'center',
+                    }),
+                }),
+            }),
+            ...rule('.initial', {
+                // positions:
+                justifySelf : 'center',
+                alignSelf   : 'center',
+                
+                
+                
+                // appearances:
+                opacity: 0.25,
+                
+                
+                
+                // sizes:
+                [iconVars.size]: `min((100svw - (2 * ${containers.paddingInline})) * 0.5, (100svh - (2 * ${containers.paddingBlock})) * 0.5)`,
+            }),
         }),
     });
 };
