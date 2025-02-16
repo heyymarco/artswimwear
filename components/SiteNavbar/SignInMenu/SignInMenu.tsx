948b336a3618260cd8f9cdec97cef1707541f615
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2025-02-16 08:00:58 +0700

    updated SignInMenu

--- components/SiteNavbar/SignInMenu/SignInMenu.tsx
+++ components/SiteNavbar/SignInMenu/SignInMenu.tsx
@@ -184,7 +184,7 @@ const SignInMenu = (props: SignInMenuProps): JSX.Element|null => {
             if ((/^\/($)|((products|categories|checkout)($|\/))/i).test(pathname)) {
                 startIntercept(async (backPathname): Promise<boolean> => {
                     toggleList(false); // collapse the <Navbar> manually
-                    router.push(signInPath); // goto signIn page
+                    router.push(signInPath, { scroll: false }); // goto signIn page, do not scroll the page because it's an interceptor signIn dialog
                     
                     
                     
