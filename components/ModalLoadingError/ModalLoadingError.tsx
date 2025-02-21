7393e2ed579e555afac09df6fde6f383a93321aa
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2025-02-21 14:18:25 +0700

    update ModalLoadingError

--- components/ModalLoadingError/ModalLoadingError.tsx
+++ components/ModalLoadingError/ModalLoadingError.tsx
@@ -17,6 +17,11 @@ import {
     dynamicStyleSheet,
 }                           from '@cssfn/cssfn-react'               // writes css in react hook
 
+// reusable-ui core:
+import {
+    // a collection of TypeScript type utilities, assertions, and validations for ensuring type safety in reusable UI components:
+    type NoForeignProps,
+}                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
 
 // reusable-ui components:
 import {
@@ -60,23 +65,67 @@ export interface ModalLoadingErrorProps<TElement extends Element = HTMLElement, 
     refetch    : () => void
 }
 export const ModalLoadingError = <TElement extends Element = HTMLElement, TModalExpandedChangeEvent extends ModalExpandedChangeEvent = ModalExpandedChangeEvent>(props: ModalLoadingErrorProps<TElement, TModalExpandedChangeEvent>): JSX.Element|null => {
-    // styles:
-    const styleSheet = useModalLoadingErrorStyleSheet();
-    
-    
-    
-    // rest props:
+    // props:
     const {
         // data:
         isFetching,
         isError,
         refetch,
-    ...restModalCardProps} = props;
+        
+        
+        
+        // other props:
+        ...restModalLoadingErrorProps
+    } = props;
+    
+    
+    
+    // styles:
+    const styles = useModalLoadingErrorStyleSheet();
     
     
     
     // refs:
     const buttonRetryRef = useRef<HTMLButtonElement|null>(null);
+    
+    
+    
+    // default props:
+    const {
+        // variants:
+        theme         = (isError ? 'danger' : undefined),
+        backdropStyle = 'interactive',
+        
+        
+        
+        // states:
+        expanded      = true,
+        
+        
+        
+        // auto focusable:
+        autoFocusOn   = (isError ? buttonRetryRef : undefined),
+        autoFocus     =  isError,
+        
+        
+        
+        // children:
+        children      = (
+            <CardBody
+                // classes:
+                className={styles.main}
+            >
+                {isFetching && <MessageLoading />}
+                
+                {isError    && <MessageError buttonRetryRef={buttonRetryRef} onRetry={refetch} />}
+            </CardBody>
+        ),
+        
+        
+        
+        // other props:
+        ...restModalCardProps
+    } = restModalLoadingErrorProps satisfies NoForeignProps<typeof restModalLoadingErrorProps, ModalCardProps<TElement, TModalExpandedChangeEvent>>;
     
     
     
@@ -90,27 +139,21 @@ export const ModalLoadingError = <TElement extends Element = HTMLElement, TModal
             
             
             // variants:
-            theme={props.theme ?? (isError ? 'danger' : undefined)}
-            backdropStyle={props.backdropStyle ?? 'interactive'}
+            theme={theme}
+            backdropStyle={backdropStyle}
             
             
             
             // states:
-            expanded={props.expanded ?? true}
+            expanded={expanded}
             
             
             
             // auto focusable:
-            autoFocusOn={props.autoFocusOn ?? (isError ? buttonRetryRef : undefined)}
+            autoFocusOn={autoFocusOn}
+            autoFocus={autoFocus}
         >
-            <CardBody
-                // classes:
-                className={styleSheet.main}
-            >
-                {isFetching && <MessageLoading />}
-                
-                {isError    && <MessageError buttonRetryRef={buttonRetryRef} onRetry={refetch} />}
-            </CardBody>
+            {children}
         </ModalCard>
     );
 };
