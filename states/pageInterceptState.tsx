c40e82a1b37753d36541611fd1ef4137999ad722
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2025-02-25 21:50:40 +0700

    refactor PageInterceptStateProvider to use state for pathname signals

--- states/pageInterceptState.tsx
+++ states/pageInterceptState.tsx
@@ -14,7 +14,6 @@ import {
     useContext,
     useMemo,
     useState,
-    useRef,
 }                           from 'react'
 
 // next-js:
@@ -81,6 +80,8 @@ const PageInterceptStateProvider = (props: React.PropsWithChildren<PageIntercept
     const originPathname: string|null = originPathnameStack?.[0] ?? null;
     const nonInterceptedPathname = (originPathname ?? mayInterceptedPathname);
     
+    const [signalPathnameUpdated] = useState<Map<string, (() => void)[]>>(() => new Map<string, (() => void)[]>());
+    
     
     
     // utilities:
@@ -91,7 +92,6 @@ const PageInterceptStateProvider = (props: React.PropsWithChildren<PageIntercept
     // actions:
     const router = useRouter();
     
-    const signalPathnameUpdated = useRef<(() => void)|undefined>(undefined);
     const restorePathnameAsync = useEvent(async (originPathname: string): Promise<void> => {
         if (originPathname.toLowerCase() === mayInterceptedPathname.toLowerCase()) return; // already the same => ignore
         
@@ -99,19 +99,20 @@ const PageInterceptStateProvider = (props: React.PropsWithChildren<PageIntercept
         
         // wait until the router is fully applied:
         const { promise: routerUpdatedPromise, resolve: routerUpdatedSignal } = Promise.withResolvers<void>();
-        signalPathnameUpdated.current = routerUpdatedSignal;
+        const signals = signalPathnameUpdated.get(originPathname) ?? (() => {
+            const newSignals : (() => void)[] = [];
+            signalPathnameUpdated.set(originPathname, newSignals);
+            return newSignals;
+        })();
+        signals.push(routerUpdatedSignal); // register the signal for the desired pathname update
         
         router.push(originPathname, { scroll: false }); // go back to unintercepted pathName // do not scroll the page because it restores the unintercepted pathName
         
         await Promise.race([
             routerUpdatedPromise,
-            setTimeoutAsync(100),
+            setTimeoutAsync(1000), // assumes if the router is not updated within 1 second, it's failed
         ]);
     });
-    useIsomorphicLayoutEffect(() => {
-        signalPathnameUpdated.current?.(); // signal updated
-        signalPathnameUpdated.current = undefined;
-    }, [mayInterceptedPathname]);
     
     const startIntercept = useEvent<PageInterceptState['startIntercept']>(async (callback) => {
         // stack up:
@@ -125,6 +126,28 @@ const PageInterceptStateProvider = (props: React.PropsWithChildren<PageIntercept
             setOriginPathnameStack((current) => current.length ? current.slice(0, -1) : current); // remove the last item (if any)
         } // try
     });
+    
+    
+    
+    // effects:
+    useIsomorphicLayoutEffect(() => {
+        const signals = signalPathnameUpdated.get(mayInterceptedPathname);
+        if (signals?.length) { // the pathname changes due to the interception
+            // signal all the matched signals:
+            for (const signal of signals) signal(); // signal updated
+            signalPathnameUpdated.delete(mayInterceptedPathname); // remove the resolved signals
+        }
+        else { // the pathname changes due to the user's action
+            // signal all the signals:
+            for (const signals of signalPathnameUpdated.values()) {
+                for (const signal of signals) signal(); // signal updated
+            } // for
+            signalPathnameUpdated.clear(); // remove all the signals
+            
+            // assumes all the interception is done:
+            setOriginPathnameStack([]);
+        } // if
+    }, [mayInterceptedPathname]);
     
     
     
