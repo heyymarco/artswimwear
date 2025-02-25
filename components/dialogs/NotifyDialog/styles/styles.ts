d6f87fcc8df3261ffb0d65abc1461066236d8f03
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2025-02-25 16:44:56 +0700

    add grid layout to NotifyDialog styles

--- components/dialogs/NotifyDialog/styles/styles.ts
+++ components/dialogs/NotifyDialog/styles/styles.ts
@@ -17,6 +17,40 @@ export default () => [
         // children:
         ...children('.body', {
             overflow: 'hidden', // prevents an ugly scrollbar shown when collapsing
+            
+            
+            
+            // children:
+            ...children('.wrapper', {
+                // layouts:
+                display: 'grid',
+                gridTemplate: [[
+                    '"icon content control" auto',
+                    '/',
+                    'max-content 1fr max-content',
+                ]],
+                alignItems: 'start',
+                
+                
+                
+                // children:
+                ...children(['.icon', '.control'], {
+                    // spacings:
+                    marginBlockEnd: 0,
+                }),
+                ...children('.icon', {
+                    // positions:
+                    gridArea: 'icon',
+                }),
+                ...children('.content', {
+                    // positions:
+                    gridArea: 'content',
+                }),
+                ...children('.control', {
+                    // positions:
+                    gridArea: 'control',
+                }),
+            }),
         }),
     }, { specificityWeight: 2 }),
 ];
